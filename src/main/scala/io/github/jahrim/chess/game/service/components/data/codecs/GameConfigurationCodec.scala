7c48881daea290ee102eab1da1fba06aca2c8ce2
madina9229 <madina.kentpayeva@studio.unibo.it>
2023-07-30 13:01:37 +0200

    chore: add BsonEncoder and BsonDecoder for GameConfigurationData

--- src/main/scala/io/github/jahrim/chess/game/service/components/data/codecs/GameConfigurationCodec.scala
+++ src/main/scala/io/github/jahrim/chess/game/service/components/data/codecs/GameConfigurationCodec.scala
@@ -4,16 +4,17 @@ import io.github.jahrim.hexarc.persistence.bson.codecs.{BsonDocumentDecoder, Bso
 import io.github.jahrim.hexarc.persistence.bson.dsl.BsonDSL.{*, given}
 import org.bson.conversions.Bson
 import io.github.jahrim.chess.game.service.components.data.GameConfigurationData
-import TimeConstraintCodec.given
-import io.github.chess.engine.model.configuration.TimeConstraint
+import io.github.jahrim.chess.game.service.components.data.TimeConstraintData
 
+import TimeConstraintCodec.given
+import GameConfigurationCodec.given
 
 /** [[Bson]] codec for [[GameConfigurationData]]. */
 object GameConfigurationCodec:
   /** A given [[BsonDocumentDecoder]] for [[GameConfigurationData]]. */
   given bsonToGameConfiguration: BsonDocumentDecoder[GameConfigurationData] = bson =>
     GameConfigurationData(
-      timeConstraint = bson("gameConfiguration.timeConstraint").map(_.as[TimeConstraint]),
+      timeConstraint = bson("gameConfiguration.timeConstraint").map(_.as[TimeConstraintData]),
       gameId = bson("gameConfiguration.gameId").map(_.as[String]),
       isPrivate = bson.require("gameConfiguration.isPrivate").as[Boolean]
     )
@@ -21,7 +22,7 @@ object GameConfigurationCodec:
   /** A given [[BsonDocumentEncoder]] for [[GameConfigurationData]]. */
   given gameConfigurationToBson: BsonDocumentEncoder[GameConfigurationData] = gc =>
     bson {
-      gc.timeConstraint.foreach { "timeConstraint" :: tc }
-      gc.gameId.foreach { "gameId" :: tc }
+      gc.timeConstraint.foreach { "timeConstraint" :: _ }
+      gc.gameId.foreach { "gameId" :: _ }
       "private" :: gc.isPrivate
     }
