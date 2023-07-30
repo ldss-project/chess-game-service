3bf38291ac97937ba1ea97f5c61bb7fd07beb157
madina9229 <madina.kentpayeva@studio.unibo.it>
2023-07-30 13:05:39 +0200

    chore: add BsonEncoder and BsonDecoder for PlayerData

---
+++ src/main/scala/io/github/jahrim/chess/game/service/components/data/codecs/PlayerCodec.scala
@@ -0,0 +1,26 @@
+package io.github.jahrim.chess.game.service.components.data.codecs
+
+import io.github.jahrim.hexarc.persistence.bson.codecs.{
+  BsonDecoder,
+  BsonDocumentDecoder,
+  BsonDocumentEncoder,
+  BsonEncoder
+}
+import io.github.jahrim.chess.game.service.components.data.PlayerData
+import org.bson.conversions.Bson
+import io.github.jahrim.hexarc.persistence.bson.dsl.BsonDSL.{*, given}
+
+/** [[Bson]] codec for [[PlayerData]]. */
+
+object PlayerCodec:
+  /** A given [[BsonDocumentDecoder]] for [[PlayerData]]. */
+  given bsonToPlayer: BsonDocumentDecoder[PlayerData] = bson =>
+    PlayerData(
+      bson.require("username").as[String]
+    )
+
+  /** A given [[BsonDocumentEncoder]] for [[PlayerData]]. */
+  given playerToBson: BsonDocumentEncoder[PlayerData] = player =>
+    bson {
+      "username" :: player.username
+    }
