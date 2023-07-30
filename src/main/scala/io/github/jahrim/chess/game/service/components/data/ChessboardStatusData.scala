374cd8a4207d8a9bc780588dbf3b8cca94e4e43a
madina9229 <madina.kentpayeva@studio.unibo.it>
2023-07-30 13:03:37 +0200

    chore: add BsonEncoder and BsonDecoder for ChessboardStatusData

--- src/main/scala/io/github/jahrim/chess/game/service/components/data/ChessboardStatusData.scala
+++ src/main/scala/io/github/jahrim/chess/game/service/components/data/ChessboardStatusData.scala
@@ -1,23 +1,8 @@
 package io.github.jahrim.chess.game.service.components.data
 
-import io.github.chess.engine.model.moves.Move
-import io.github.chess.engine.model.pieces.Piece
 import io.github.jahrim.hexarc.persistence.bson.codecs.{BsonDocumentDecoder, BsonDocumentEncoder}
 
 case class ChessboardStatusData(
-  pieces: Seq[Piece],
-  moves: Seq[Move]
+    pieces: Seq[PieceData],
+    moves: Seq[MoveData]
 )
-
-object ChessboardStatusCodec:
-  given BsonDocumentDecoder[ChessboardStatusData] = bson =>
-    ChessboardStatusData(
-      pieces = bson.require("pieces").as[Seq[Piece]],
-      moves = bson.require("moves").as[Seq[Move]]
-    )
-
-  given BsonDocumentEncoder[ChessboardStatusData] = csd =>
-    bson {
-      "pieces" :: csd.pieces
-      "moves" :: csd.moves
-    }
