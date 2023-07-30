81f4108ed05d3a90e5d220dcc2372c967dfad483
madina9229 <madina.kentpayeva@studio.unibo.it>
2023-07-30 13:01:37 +0200

    chore: add BsonEncoder and BsonDecoder for GameConfigurationData

--- src/main/scala/io/github/jahrim/chess/game/service/components/data/GameConfigurationData.scala
+++ src/main/scala/io/github/jahrim/chess/game/service/components/data/GameConfigurationData.scala
@@ -1,9 +1,7 @@
 package io.github.jahrim.chess.game.service.components.data
 
-import io.github.chess.engine.model.configuration.TimeConstraint
-
 case class GameConfigurationData(
-  timeConstraint: Option[TimeConstraint], 
-  gameId: Option[String],
-  isPrivate: Boolean
+    timeConstraint: Option[TimeConstraintData],
+    gameId: Option[String],
+    isPrivate: Boolean
 )
