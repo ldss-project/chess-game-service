1e70534ef21a7b10f9bbdac48655cda0e93f0f4f
madina9229 <madina.kentpayeva@studio.unibo.it>
2023-07-30 18:43:25 +0200

    chore: change type to PieceData

--- src/main/scala/io/github/jahrim/chess/game/service/components/data/codecs/EnPassantCodec.scala
+++ src/main/scala/io/github/jahrim/chess/game/service/components/data/codecs/EnPassantCodec.scala
@@ -3,9 +3,9 @@ package io.github.jahrim.chess.game.service.components.data.codecs
 import org.bson.conversions.Bson
 import io.github.jahrim.chess.game.service.components.data.EnPassantData
 import io.github.jahrim.hexarc.persistence.bson.codecs.{BsonDocumentDecoder, BsonDocumentEncoder}
-import io.github.jahrim.chess.game.service.components.data.PieceTypeData
+import io.github.jahrim.chess.game.service.components.data.PieceData
 import io.github.jahrim.hexarc.persistence.bson.dsl.BsonDSL.{*, given}
-import PieceTypeDataCodec.given
+import PieceCodec.given
 import EnPassantData.given
 
 /** [[Bson]] codec for [[EnPassantData]]. */
@@ -13,7 +13,7 @@ object EnPassantCodec:
   /** A given [[BsonDocumentDecoder]] for [[EnPassantData]]. */
   given bsonToEnPassant: BsonDocumentDecoder[EnPassantData] = bson =>
     EnPassantData(
-      opponentPawn = bson.require("opponentPawn").as[PieceTypeData]
+      opponentPawn = bson.require("opponentPawn").as[PieceData]
     )
 
   /** A given [[BsonDocumentEncoder]] for [[EnPassantData]]. */
