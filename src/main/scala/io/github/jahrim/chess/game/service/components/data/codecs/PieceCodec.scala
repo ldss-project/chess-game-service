90773d1b585d9851026c0ce24a6a9e44b03fc788
madina9229 <madina.kentpayeva@studio.unibo.it>
2023-07-30 13:08:29 +0200

    chore: add BsonEncoder and BsonDecoder for PieceData

---
+++ src/main/scala/io/github/jahrim/chess/game/service/components/data/codecs/PieceCodec.scala
@@ -0,0 +1,26 @@
+package io.github.jahrim.chess.game.service.components.data.codecs
+
+import org.bson.conversions.Bson
+import io.github.jahrim.chess.game.service.components.data.{PieceData, PieceTypeData, PositionData}
+import io.github.jahrim.hexarc.persistence.bson.codecs.{BsonDocumentDecoder, BsonDocumentEncoder}
+import PieceCodec.given
+import PositionCodec.given
+import PieceTypeDataCodec.given
+import io.github.jahrim.hexarc.persistence.bson.dsl.BsonDSL.{*, given}
+
+/** [[Bson]] codec for [[PieceData]] */
+object PieceCodec:
+
+  /** A given [[BsonDocumentDecoder]] for [[PieceData]] */
+  given bsonToPiece: BsonDocumentDecoder[PieceData] = bson =>
+    PieceData(
+      pieceType = bson.require("type").as[PieceTypeData],
+      position = bson.require("position").as[PositionData]
+    )
+
+  /** A given [[BsonDocumentEncoder]] for [[PieceData]] */
+  given pieceToBson: BsonDocumentEncoder[PieceData] = piece =>
+    bson {
+      "type" :: piece.pieceType
+      "position" :: piece.position
+    }
