925c8ee7a9c32ac2789feb79677f493c66588c57
madina9229 <madina.kentpayeva@studio.unibo.it>
2023-08-25 16:23:35 +0200

    fix: update entry point of the chess game service

--- src/main/scala/io/github/jahrim/chess/game/service/main/Main.scala
+++ src/main/scala/io/github/jahrim/chess/game/service/main/Main.scala
@@ -1,14 +1,34 @@
 package io.github.jahrim.chess.game.service.main
 
 import io.github.chess.engine.model.board.{File, Rank}
-import io.github.jahrim.chess.game.service.components.data.PositionData
+import io.github.jahrim.chess.game.service.components.adapters.http.ChessGameHttpAdapter
+import io.github.jahrim.chess.game.service.components.ports.{ChessGameModel, ChessGamePort}
+import io.github.jahrim.hexarc.architecture.vertx.core.dsl.VertxDSL.*
+import io.vertx.core.Vertx
+import io.vertx.core.http.HttpServerOptions
 import org.bson.BsonDocument
 import org.rogach.scallop.*
 
 /** Main of the application. */
 @main def main(args: String*): Unit =
   val arguments: Args = Args(args)
-  println("Hello world!")
+
+  DeploymentGroup.deploySingle(Vertx.vertx()) {
+    new Service:
+      name = "ChessGameService"
+
+      new Port[ChessGamePort]:
+        model = ChessGameModel( /* TODO pass a statistics proxy */ )
+
+        new Adapter(
+          adapter = ChessGameHttpAdapter(
+            httpOptions = HttpServerOptions()
+              .setHost(arguments.httpHost())
+              .setPort(arguments.httpPort()),
+            allowedOrigins = arguments.allowedOrigins()
+          )
+        )
+  }
 
 /**
  * The parsed command line arguments accepted by this application.
@@ -18,4 +38,28 @@ import org.rogach.scallop.*
  * @see [[https://github.com/scallop/scallop Scallop Documentation on Github]].
  */
 class Args(private val arguments: Seq[String]) extends ScallopConf(arguments):
+  val statisticsService: ScallopOption[String] = opt[String](
+    name = "statistics-service",
+    descr = "The host of the statistics service where the result of the matches will be stored.",
+    default = Some("localhost:8082"),
+    required = false
+  )
+  val httpHost: ScallopOption[String] = opt[String](
+    name = "http-host",
+    descr = "The server host for the http adapter of this service.",
+    default = Some("localhost"),
+    required = false
+  )
+  val httpPort: ScallopOption[Int] = opt[Int](
+    name = "http-port",
+    descr = "The server port for the http adapter of this service.",
+    default = Some(8080),
+    required = false
+  )
+  val allowedOrigins: ScallopOption[Seq[String]] = opt[String](
+    name = "allowed-origins",
+    descr = "A list of colon separated origins that are allowed to access this service.",
+    default = Some(""),
+    required = false
+  ).map(_.split(";").toSeq)
   verify()
